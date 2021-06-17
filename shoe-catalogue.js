92cf3a9d6e091d44bf60e7781e6cf1df9b541848
Jaden Hope <Jaden_Hope@Yahoo.com>
2021-06-17 11:43:57 +0200

    All buttons functioning, i think

--- shoe-catalogue.js
+++ shoe-catalogue.js
@@ -8,9 +8,9 @@ const addStock = document.getElementById("stock");
 const addPrice = document.getElementById("price");
 const searchBtn = document.getElementById("searchBtn");
 const cancelCartBtn = document.getElementById("cartCancel");
-const removeCartBtn = document.getElementById("cartCancel");
+const removeCartBtn = document.getElementById("cartRemove");
 const addToCartBtn = document.getElementById("cartAdd");
-const purchaseBtn = document.getElementById("purchaseBtn");
+const purchaseBtn = document.querySelector(".purchaseBtn");
 const cartBtn = document.getElementById("cartBtn");
 const addStockBtn = document.querySelector(".addStockBtn");
 const popupStock = document.querySelector(".popup");
@@ -74,6 +74,10 @@ cartAdd.addEventListener("click", () => {
         cartList = JSON.parse(localStorage.getItem("cart"));
     }
 
+    if (localStorage["shoes"]) {
+        shoeList = JSON.parse(localStorage.getItem("shoes"));
+    }
+
     var flagCart = false;
 
     if (cartList.length) {
@@ -135,6 +139,74 @@ cartBtn.addEventListener("click", () => {
     } else {
         cartForm.style.display = "none";
     }
+})
+
+cancelCartBtn.addEventListener("click", () => {
+    if (localStorage["shoes"]) {
+        shoeList = JSON.parse(localStorage.getItem("shoes"));
+    }
+    if (localStorage["cart"]) {
+        cartList = JSON.parse(localStorage.getItem("cart"));
+    }
+
+    for (var k = 0; k < cartList.length; k++) {
+        var currItem = cartList[k];
+        for (var i = 0; i < shoeList.length; i++) {
+            var currShoe = shoeList[i];
+            if ((currShoe.brand === currItem.brand) && (currShoe.size === currItem.size) && (currShoe.color === currItem.color)) {
+                currShoe.stock += currItem.quantity;
+            }
+        }
+    }
+
+    cartTableElem.innerHTML = "<tr><th>Brand:</th><th>Size:</th><th>Color:</th><th>Quantity:</th>";
+    localStorage.setItem("shoes", JSON.stringify(shoeList));
+    cartList = [];
+    localStorage.removeItem("cart");
+})
+
+removeCartBtn.addEventListener("click", () => {
+    if (localStorage["cart"]) {
+        cartList = JSON.parse(localStorage.getItem("cart"));
+    }
+    if (localStorage["shoes"]) {
+        shoeList - JSON.parse(localStorage.getItem("shoes"));
+    }
+
+    for (var k = 0; k < cartList.length; k++) {
+        var currItem = cartList[k];
+        if ((currItem.brand === shoeBrand.value) && (currItem.size === shoeSize.value) && (currItem.color === shoeColor.value)) {
+            for (var i = 0; i < shoeList.length; i++) {
+                var currShoe = shoeList[i];
+                if ((currShoe.brand === currItem.brand) && (currShoe.size === currItem.size) && (currShoe.color === currItem.color)) {
+                    console.log(currShoe);
+                    console.log(currItem);
+                    currShoe.stock += currItem.quantity;
+                }
+            }
+            cartList.splice(k, 1);
+        }
+    }
+
+    if (cartList.length) {
+        cartTableElem.innerHTML = "<tr><th>Brand:</th><th>Size:</th><th>Color:</th><th>Quantity:</th>";
+
+        for (var i = 0; i < cartList.length; i++) {
+            currItem = cartList[i];
+            var displayHTML = '';
+            displayHTML = compCartTemplate({ shoeBrand: currItem.brand, shoeSize: currItem.size, shoeColor: currItem.color, shoeQuantity: currItem.quantity });
+            cartTableElem.innerHTML += displayHTML;
+        }
+    }
+    console.log(cartList);
+    localStorage.setItem("cart", JSON.stringify(cartList));
+    localStorage.setItem("shoes", JSON.stringify(shoeList));
+})
+
+purchaseBtn.addEventListener('click', () => {
+    cartTableElem.innerHTML = "<tr><th>Brand:</th><th>Size:</th><th>Color:</th><th>Quantity:</th>";
+    cartList = [];
+    localStorage.removeItem("cart");
 })
 
 addStockBtn.addEventListener("click", () => {
